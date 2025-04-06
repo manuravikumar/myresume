c7395391ccdb61e7438039f15af57491159848f1
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-04-06 20:08:27 +1000

    Add approval step to deployment workflow and update storage account name

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "resume" {
 }
 
 resource "azurerm_storage_account" "resume" {
-  name                     = "resumestorageacct"
+  name                     = "manuresumestorageacct"
   resource_group_name      = azurerm_resource_group.resume.name
   location                 = azurerm_resource_group.resume.location
   account_tier             = "Standard"
