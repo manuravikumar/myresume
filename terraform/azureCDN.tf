95f34e289505b338aad88cfe7c413bec2e58cd66
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-04-20 15:56:27 +1000

    adding cdn

--- terraform/azureCDN.tf
+++ terraform/azureCDN.tf
@@ -1,6 +1,6 @@
 resource "azurerm_cdn_profile" "resume_cdn" {
   name                = "cdnprofile-resume"
-  location            = "Global"
+  location            = azurerm_resource_group.resume.location
   resource_group_name = azurerm_resource_group.resume.name
   sku                 = "Standard_Microsoft"
 }
@@ -8,7 +8,7 @@ resource "azurerm_cdn_profile" "resume_cdn" {
 resource "azurerm_cdn_endpoint" "resume_cdn_endpoint" {
   name                = "cdnendpoint-resume"
   profile_name        = azurerm_cdn_profile.resume_cdn.name
-  location            = "Global"
+  location            = azurerm_resource_group.resume.location
   resource_group_name = azurerm_resource_group.resume.name
 
   origin {
@@ -20,16 +20,4 @@ resource "azurerm_cdn_endpoint" "resume_cdn_endpoint" {
   is_https_allowed = true
 }
 
-resource "azurerm_cdn_custom_domain" "resume_custom_domain" {
-  name                = "customdomain-resume"
-  resource_group_name = azurerm_resource_group.resume.name
-  profile_name        = azurerm_cdn_profile.resume_cdn.name
-  endpoint_name       = azurerm_cdn_endpoint.resume_cdn_endpoint.name
-  host_name           = "www.manuravikumar-cloudengineer.com" # Replace
-}
 
-resource "azurerm_cdn_frontdoor_custom_domain_https" "resume_https" {
-  custom_domain_id = azurerm_cdn_custom_domain.resume_custom_domain.id
-  protocol_type    = "ServerNameIndication"
-  certificate_type = "ManagedCertificate"
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,11 @@ output "static_website_url" {
 output "cdn_url" {
   value = "https://${azurerm_cdn_endpoint.resume_cdn_endpoint.host_name}"
 }
-
-output "storage_url" {
-  value = azurerm_storage_account.resume.primary_web_endpoint
+output "cdn_endpoint" {
+  description = "The CDN endpoint for the static website"
+  value       = azurerm_cdn_endpoint.resume_cdn_endpoint.host_name
+}
+output "cdn_profile" {
+  description = "The CDN profile for the static website"
+  value       = azurerm_cdn_profile.resume_cdn.name
 }
