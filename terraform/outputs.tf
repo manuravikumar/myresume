f62ad0f3ac0992bd03dc5698a6f13956011fff56
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-04-20 15:58:29 +1000

    remove unused CDN output variables from Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,14 +2,3 @@ output "static_website_url" {
   description = "The URL of the static resume website"
   value       = azurerm_storage_account.resume.primary_web_endpoint
 }
-output "cdn_url" {
-  value = "https://${azurerm_cdn_endpoint.resume_cdn_endpoint.host_name}"
-}
-output "cdn_endpoint" {
-  description = "The CDN endpoint for the static website"
-  value       = azurerm_cdn_endpoint.resume_cdn_endpoint.host_name
-}
-output "cdn_profile" {
-  description = "The CDN profile for the static website"
-  value       = azurerm_cdn_profile.resume_cdn.name
-}
