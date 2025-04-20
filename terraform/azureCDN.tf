928ae4cb80b34170776f3562973527d60bee393f
manuravikumar <65894900+manuravikumar@users.noreply.github.com>
2025-04-20 16:02:00 +1000

    refactor: format origin block in CDN endpoint for improved readability

--- terraform/azureCDN.tf
+++ terraform/azureCDN.tf
@@ -11,10 +11,15 @@ resource "azurerm_cdn_endpoint" "resume_cdn_endpoint" {
   location            = azurerm_resource_group.resume.location
   resource_group_name = azurerm_resource_group.resume.name
 
-  origin {
-    name      = "blobstaticorigin"
-    host_name = replace(azurerm_storage_account.resume.primary_web_endpoint, "https://", "")
-  }
+ origin {
+  name      = "blobstaticorigin"
+  host_name = replace(
+    replace(azurerm_storage_account.resume.primary_web_endpoint, "https://", ""),
+    "/",
+    ""
+  )
+}
+
 
   is_http_allowed  = false
   is_https_allowed = true
